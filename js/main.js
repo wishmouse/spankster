a7437b854035550b1731529397a4d1cbef048475
Heidi Goosen <Heidi Goosen>
2017-01-15 13:49:08 +1300

    adding adult naughty

--- js/main.js
+++ js/main.js
@@ -28,50 +28,163 @@ function randomNumber() {
 };
 
 
-/*
-switch (newpunishment) {
-  case <= 0.1:
-    return "punishment1";
-    break;
-  case <= 0.2:
-    return "punishment2";
-    break;
-  case <= 0.3:
-    return "punishment3";
-    break;
-  case <= 0.4:
-    return "punishment4";
-    break;
-  case <= 0.5:
-    return "punishment5";
-    break;
-  case <= 0.6:
-    return "punishment6";
-    break;
-  case <= 0.7:
-    return "punishment7";
-    break;
-  case <= 0.8:
-    return "punishment8";
-    break;
-  case <= 0.9:
-    return "punishment9";
-    break;
-  case <= 1:
-    return "punishment10";
-    break;
+function randomNumberAdult() {
+   var mathRandom =  Math.floor(Math.random()* 72+1);
 
-}
+   document.body.appendChild(countdown).play()
 
+ if (mathRandom === 1) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Blindfolds and feather dusters";}, 5000);
+  } else if (mathRandom === 2) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Elasic band snap on your nipples.";}, 5000);
+  } else if (mathRandom === 3) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Diet be damned, get creative with ice-cream";}, 5000);
+  } else if (mathRandom === 4) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Dance puppet dance!";}, 5000);
+  } else if (mathRandom === 5) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Wear a gag";}, 5000);
+  } else if (mathRandom === 6) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Play with vibrators, I'll watch";}, 5000);
+  } else if (mathRandom === 7) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Its time to wear the beads";}, 5000);
+  } else if (mathRandom === 8) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="A really good spanking";}, 5000);
+  } else if (mathRandom === 9) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Tie him to the bed and give him a blowjob ";}, 5000);
+  } else if (mathRandom === 10) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Butt plugs are an ass lover’s best friend";}, 5000);
+  } else if (mathRandom === 11) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Just the tip and see how it feels";}, 5000);
+  } else if (mathRandom === 12) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Get down on your knees, crawl to me, unbutton my ....";}, 5000);
+  } else if (mathRandom === 13) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Watch kinky porn... now copy";}, 5000);
+  } else if (mathRandom === 14) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Choke me";}, 5000);
+  } else if (mathRandom === 15) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Demand all oral sex – and nothing else";}, 5000);
+  } else if (mathRandom === 16) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Pick up a stranger and make out with them.";}, 5000);
+  } else if (mathRandom === 17) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Slide your cock between my breasts.";}, 5000);
+  } else if (mathRandom === 18) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Have him wear a cock ring";}, 5000);
+  } else if (mathRandom === 19) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Lead your lover around by a leash";}, 5000);
+  } else if (mathRandom === 20) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Have sex on the balcony";}, 5000);
+  } else if (mathRandom === 21) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Take out the nipple clamps and let the games begin";}, 5000);
+  } else if (mathRandom === 22) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Keep your panties on ";}, 5000);
+  } else if (mathRandom === 23) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Wear high heels to bed";}, 5000);
+  } else if (mathRandom === 24) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Host an 'interrogation' using pleasure and pain";}, 5000);
+  } else if (mathRandom === 25) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Violet Wand";}, 5000);
+  } else if (mathRandom === 26) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Make her squirt";}, 5000);
+  } else if (mathRandom === 27) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Shower sex";}, 5000);
+  } else if (mathRandom === 28) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Sex in the kitchen";}, 5000);
+  } else if (mathRandom === 29) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Sensation play";}, 5000);
+  } else if (mathRandom === 30) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Hot wax and cloths pegs";}, 5000);
+  } else if (mathRandom === 31) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Bend over a chair and be spanked ";}, 5000);
+  } else if (mathRandom === 73) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Tie your partner up and get freaky on them with a vibrator";}, 5000);
+  } else if (mathRandom === 72) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Sex in the living room";}, 5000);
+  } else if (mathRandom === 32) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="I love it when you kiss me... especially there";}, 5000);
+  } else if (mathRandom === 33) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Lick cum from your fingers";}, 5000);
+  }else if (mathRandom === 34) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Suprise your partner with roleplay";}, 5000);
+  }else if (mathRandom === 35) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Suck on your lover’s balls while he jacks off.";}, 5000);
+  }else if (mathRandom === 36) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Suck their toes";}, 5000);
+  }else if (mathRandom === 37) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Wear lingerie";}, 5000);
+  }else if (mathRandom === 38) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Torture your lover with ice ";}, 5000);
+  }else if (mathRandom === 38) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Have sex in the same room with another couple and watch each other.";}, 5000);
+  }else if (mathRandom === 39) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Find his prostate and massage it until he cums";}, 5000);
+  }else if (mathRandom === 40) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Slide pieces of fruit into your lover and suck them out.";}, 5000);
+  }else if (mathRandom === 41) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Slip your finger up your man’s ass right as he’s about to cum";}, 5000);
+  }else if (mathRandom === 42) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Needle play";}, 5000);
+  }else if (mathRandom === 43) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Have sex in the parking lot ";}, 5000);
+  }else if (mathRandom === 44) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Go down on your man in a crowded place";}, 5000);
+  }else if (mathRandom === 45) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Do it in a public park";}, 5000);
+  }else if (mathRandom === 46) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Use toys to find her g-spot ";}, 5000);
+  }else if (mathRandom === 47) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Do your Kegel exercises while he is inside you";}, 5000);
+  }else if (mathRandom === 48) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Use secutions on her nipples";}, 5000);
+  }else if (mathRandom === 49) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Use secutions on her clit";}, 5000);
+  }else if (mathRandom === 50) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Play with a pussy pocket / flesh light / grapefruit";}, 5000);
+  }else if (mathRandom === 70) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="69";}, 5000);
+  }else if (mathRandom === 71) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Cum on her face";}, 5000);
+  }else if (mathRandom === 51) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Use toys, take pictures";}, 5000);
+  }else if (mathRandom === 52) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Sext.. when you are sitting next to each other";}, 5000);
+  }else if (mathRandom === 53) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Have sex where there is a chance of being caught";}, 5000);
+  }else if (mathRandom === 54) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Spontaneous sex";}, 5000);
+  }else if (mathRandom === 55) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Tiger Balm ..use sparingly";}, 5000);
+  }else if (mathRandom === 56) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Spreader bars and a vibrator";}, 5000);
+  }else if (mathRandom === 57) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Air tight";}, 5000);
+  }else if (mathRandom === 58) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Slap across the face";}, 5000);
+  }else if (mathRandom === 59) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Tie up his cock and balls";}, 5000);
+  }else if (mathRandom === 60) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Nibble on a strangers earlobe";}, 5000);
+  }else if (mathRandom === 61) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Lapdance. Naked or in a public place ";}, 5000);
+  }else if (mathRandom === 62) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="xxx";}, 5000);
+  }else if (mathRandom === 63) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="xxx";}, 5000);
+  }else if (mathRandom === 64) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="xxx";}, 5000);
+  }else if (mathRandom === 65) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="xxx";}, 5000);
+  }else if (mathRandom === 66) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="xxx";}, 5000);
+  }else if (mathRandom === 67) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="xxx";}, 5000);
+  }else if (mathRandom === 68) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="xxx";}, 5000);
+  }else if (mathRandom === 69) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="xxx";}, 5000);
+  } else if (mathRandom === 74) {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="xxx";}, 5000);
+  } else {
+    setTimeout(function(){document.getElementById("punishment").innerHTML="Make Heidi tea.";}, 5000);
+  }
 
-
-//$(document).ready(function(){
-    //button click
-    //$("button").click(function(){
-        //$("p:first").replaceWith("\"Life is uncertain. Eat dessert first.\" -Ernestine Ulmer");
-    //});
-
-
-
-//});//end of document ready function
-*/
+};
